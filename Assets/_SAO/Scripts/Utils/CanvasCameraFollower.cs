3e76facd42b2f5e5f4ae21b3bb53f60ec35a175f
l.hilgevoord <l.hilgevoord@azerion.com>
2021-05-29 13:40:56 +0200

    Canvas camera follower lookat fix

--- Assets/_SAO/Scripts/Utils/CanvasCameraFollower.cs
+++ Assets/_SAO/Scripts/Utils/CanvasCameraFollower.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class CanvasCameraFollower : MonoBehaviour
 {
     [SerializeField] private Camera playerCamera;
-    private float distance;
-    private float lerpSpeed = 0.3f;
+    [SerializeField] private float distance = 2;
+    [SerializeField] private float lerpSpeed = 0.05f;
 
-    private void Start()
-    {
-        distance = Vector3.Distance(playerCamera.transform.position, this.transform.position);
-    }
+    private float maxHeightDistance = 1;
 
-    private void Update()
+    private void FixedUpdate()
     {
         Vector3 newPos = playerCamera.transform.position + playerCamera.transform.forward * distance;
         this.transform.position = Vector3.Lerp(this.transform.position, newPos, lerpSpeed);
 
-        //this.transform.LookAt(playerCamera.transform, Vector3.up);
+        this.transform.LookAt(2 * transform.position - playerCamera.transform.position);
     }
 }
